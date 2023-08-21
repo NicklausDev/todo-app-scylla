0c633d335df58627bd6a34232912e316256b98e7
NicklausDev <nicklauswandera@gmail.com>
2023-08-21 03:50:29 +0300

    Setup API

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3001;
 
 app.get('/', (req, res) => {
     res.json ({
         message: "Welcome to ScyllaDB"
     });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to https://localhost:${PORT}`);
 });
