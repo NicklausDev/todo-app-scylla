43d900335195e63e5345a716481d338b8ade3af6
NicklausDev <nicklauswandera@gmail.com>
2023-08-23 15:12:50 +0300

    Finished Front-End Setup and Connection to Back-end

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -6,12 +6,18 @@ const TodoList = () => {
   const [items, setItems] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/items', { crossdomain: true })
-      .then((res) => console.log(res))
-  });
+    axios.get('http://localhost:3001/api/items')
+      .then((res) => setItems(res.data.items));
+  }, []);
+
+  if (items === null) return <div>loading...</div>
 
   return (
-    <div>TodoList</div>
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
   )
 };
 
